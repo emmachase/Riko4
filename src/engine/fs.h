69b6e100638129e83dfaff156504a64429322b63
Incinirate <dogescript@gmail.com>
2020-04-19 17:59:03 -0600

    Rewrite checkpath, fixes #34

--- src/engine/fs.h
+++ src/engine/fs.h
@@ -7,8 +7,10 @@
 #ifdef __WINDOWS__
 #  define getFullPath(a, b) GetFullPathName(a, MAX_PATH, b, NULL)
 #  define rmdir(a) _rmdir(a)
+#  define PATH_SEPARATOR '\\'
 #else
 #  define getFullPath(a, b) realpath(a, b)
+#  define PATH_SEPARATOR '/'
 #endif
 
 namespace riko::fs {
